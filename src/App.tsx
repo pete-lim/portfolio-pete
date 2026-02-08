e33022aa5be0483f070547f56c88ebf2427ba94f
Pete <134797466+pete-lim@users.noreply.github.com>
2026-02-08 16:45:16 +0800

    -feat Hero section
    Implemented hero section, focused on mobile first development
    
    Incorporated modular css styling for reuse
    
    Placeholder group image, placeholder banner to be replaced with hamburg icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
+import '@fontsource/inter/400.css';
 import './App.css';
-
+import HeroSection from './components/Hero';
 function App() {
-  return <> hi</>;
+  const [theme, setTheme] = useState('light-mode');
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+  return (
+    <div className="root">
+      <div className="fixed inset-0 -z-10 bg"></div>
+      <span className="fixed w-screen h-1/10 flex justify-around">
+        <button>Home</button>
+        <button>Tech</button>
+        <button>Work</button>
+        <button>Projects</button>
+        <button>Contact</button>
+      </span>
+      <HeroSection />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,26 @@
+import heroPhoto from '../assets/images/hero-photo.png';
+import { HiLocationMarker } from 'react-icons/hi';
+import { FiChevronDown } from 'react-icons/fi';
+export default function HeroSection() {
+  return (
+    <div className="section">
+      <header className="mb-4">Hi, I'm Pete</header>
+      <span className="flex">
+        <h2>A Graduate</h2>
+        <h2 className="text-primary! pl-1">Software Engineer,</h2>
+      </span>
+      <span className="flex items-center">
+        <h2>South Australia, Adelaide</h2>
+        <HiLocationMarker className="ml-1 text-red-500" />
+      </span>
+      <img
+        src={heroPhoto}
+        alt=""
+        className="mix-blend-darken mt-10 w-full h-5/10 object-cover"
+      />
+      <button className="absolute bottom-10 left-[49%]">
+        <FiChevronDown className="text-xl text-accent" />
+      </button>
+    </div>
+  );
+}
