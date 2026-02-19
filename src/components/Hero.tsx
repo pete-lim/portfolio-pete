67872b036bc0be112eb8eecbee94d3dccf01346d
Pete <petedaniel.lim@gmail.com>
2026-02-19 15:18:09 +1030

    -style distortion text
    Framer motion value distort text on hover for hero section.
    
    Allows type of tag and custom class. Cane be transitioned to an external class for dynamic usage

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,62 @@
 import heroPhoto from '../assets/images/hero-photo.png';
 import { HiLocationMarker } from 'react-icons/hi';
 import { FiChevronDown } from 'react-icons/fi';
+import { motion, useMotionValue, useSpring } from 'framer-motion';
+import type { ElementType, ReactNode } from 'react';
+
+type prop = {
+  children: ReactNode;
+  tag?: ElementType;
+  className?: string;
+};
+function DistortText({ children, tag = 'h2', className = '' }: prop) {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const springX = useSpring(x, { stiffness: 150, damping: 12 });
+  const springY = useSpring(y, { stiffness: 150, damping: 12 });
+
+  function onMove(e: React.MouseEvent<HTMLHeadingElement, MouseEvent>) {
+    const box = e.currentTarget.getBoundingClientRect();
+    const relX = e.clientX - (box.left + box.width / 2);
+    const relY = e.clientY - (box.top + box.height / 2);
+    x.set(relX * 0.4);
+    y.set(relY * 0.4);
+  }
+  type allowedTags = 'header' | 'h2' | 'h3';
+  const MotionTag = motion[tag as allowedTags];
+  return (
+    <MotionTag
+      className={`distort-word ${className}`}
+      onMouseMove={onMove}
+      onMouseLeave={() => {
+        x.set(0);
+        y.set(0);
+      }}
+      style={{ x: springX, y: springY }}
+    >
+      {children}
+    </MotionTag>
+  );
+}
+
 export default function HeroSection() {
   return (
     <section className="section" id="home">
-      <header className="mb-4">Hi, I'm Pete</header>
-      <span className="flex">
-        <h2>A Graduate</h2>
-        <h2 className="text-primary! pl-1">Software Engineer,</h2>
+      <span className="flex gap-2 mb-4">
+        <DistortText tag="header">Hi,</DistortText>
+        <DistortText tag="header">I'm</DistortText>
+        <DistortText tag="header">Pete</DistortText>
       </span>
-      <span className="flex items-center">
-        <h2>South Australia, Adelaide</h2>
+      <span className="flex gap-4">
+        <DistortText>A</DistortText>
+        <DistortText>Graduate</DistortText>
+        <DistortText tag="h3">Software</DistortText>
+        <DistortText tag="h3">Engineer,</DistortText>
+      </span>
+      <span className="flex items-center gap-4">
+        <DistortText>South</DistortText>
+        <DistortText>Australia,</DistortText>
+        <DistortText>Adelaide</DistortText>
         <HiLocationMarker className="ml-1 text-red-500" />
       </span>
       <img
